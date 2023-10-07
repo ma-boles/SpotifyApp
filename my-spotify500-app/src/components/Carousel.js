f31861c9e1378b585d5b9f39628bcd9c5f431660
ma-boles <ma_boles@hotmail.com>
2023-10-06 18:17:46 -0700

    git commit -m "border radius updates"

--- my-spotify500-app/src/components/Carousel.js
+++ my-spotify500-app/src/components/Carousel.js
@@ -10,7 +10,7 @@ export default function Carousel( ) {
 const slideStyles = {
   width: "100%",
   height: "100%",
-  borderRadius: "10px",
+  /*borderRadius: "10px",*/
   backgroundSize: "cover",
   backgroundPosition: "center",
 };
