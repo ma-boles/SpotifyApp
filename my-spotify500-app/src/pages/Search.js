dc21a73c93938fbe96d9a826a85036ceb5154c30
ma-boles <ma_boles@hotmail.com>
2023-10-12 18:25:57 -0700

    git commit -m "added link mobile search"

--- my-spotify500-app/src/pages/Search.js
+++ my-spotify500-app/src/pages/Search.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Search.css";
 import right from "../images/chevron-right-solid.svg";
 import left from "../images/chevron-left-solid.svg";
@@ -7,30 +8,18 @@ import Album from "../components/Album"
 
 export default function Search() {
 
-    const [ isBrowseVisible, setIsBrowseVisible ] = useState(true);
-
-    const toggleBrowse = () => {
-        setIsBrowseVisible(!isBrowseVisible);
-    };
-    
     return (
         <>
         <section className="search">
 
             <div className="search--mobile--div">
                 <h2 className="search--mobile--h2">Search</h2>
-                <button className="search--mobile--input" onClick={toggleBrowse}>
-                    {isBrowseVisible ? "blue" :
-                     <input className="search--input" placeholder="What do you want to listen to?">
-                     </input>}
-                    </button>
-                    {isBrowseVisible && (
-                        <input className="search--input" placeholder="What do you want to listen to?">
-                        </input>
-                    )}
-                    
-
-            </div>
+                   <Link to="../MobileSearch" className="link--mobileSearch">
+                        <button className="search--mobile--input">
+                        "What do you want to listen to?
+                        </button>
+                    </Link>
+          </div>
             <nav className="search--nav">
                     <img src={left} className='search--arrowLeft'></img>
                     <img src={right} className='search--arrowRight'></img>
