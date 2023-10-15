b566996ab93be2b96e3167feb0f7436ec090baac
ma-boles <ma_boles@hotmail.com>
2023-10-14 17:27:51 -0700

    git commit -m "new footer component"

---
+++ my-spotify500-app/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <>
+                <p className="footer--p">&copy; 2023 mab.dev</p>
+
+        </>
+    )
+}
