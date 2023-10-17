f69cbfd54f63d2436c04927c18bb3f77ff448c37
ma-boles <ma_boles@hotmail.com>
2023-10-16 18:07:24 -0700

    git commit -m "added hero image"

--- my-spotify500-app/src/pages/HomePage.js
+++ my-spotify500-app/src/pages/HomePage.js
@@ -1,16 +1,25 @@
 import React from "react";
-import Carousel from "../components/Carousel"; 
 import Album from "../components/Album";
+import HeaderImg from "../images/HomeLogo.jpg";
+
 import "../styles/HomePage.css";
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
+
+    const slideStyles = {
+        width: "100%",
+        height: "100%",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      };
+
     return(
         <>
         <section className='homePage'>
             <div className="home">
-            
-                <Carousel />
+
+            <img src={HeaderImg} style={slideStyles}></img>
 
             </div>
 
