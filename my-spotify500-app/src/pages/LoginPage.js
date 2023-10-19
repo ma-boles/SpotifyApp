0a93013276d23788621b1bae3b10cfd73233d4b8
ma-boles <ma_boles@hotmail.com>
2023-10-18 17:43:31 -0700

    git commit -m "updated login page"

--- my-spotify500-app/src/pages/LoginPage.js
+++ my-spotify500-app/src/pages/LoginPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/App.css";
+import "../styles/Login.css"
 import { Routes, Route, Link } from "react-router-dom";
 import LoginCard from "../components/LoginCard";
 import Login from "../components/Login"
@@ -8,8 +9,8 @@ import Login from "../components/Login"
 export default function LoginPage() {
     return(
         <>
-            <Login />
-            <LoginCard />
+{            /*<Login />*/
+}            <LoginCard />
        </>
     )
 }
