08821403bf1a44cd412243ace51c721a9ee34c9a
ma-boles <ma_boles@hotmail.com>
2023-10-20 14:36:52 -0700

    git commit -m "added routes for Top500 links"

--- my-spotify500-app/src/App.js
+++ my-spotify500-app/src/App.js
@@ -11,6 +11,14 @@ import Search from './pages/Search';
 import LoginPage from './pages/LoginPage';
 import MobileSearch from './pages/MobileSearch';
 
+import Top500_2 from './pages/Top500_2';
+import Top500_3 from './pages/Top500_3';
+import Top500_4 from './pages/Top500_4';
+import Top500_5 from './pages/Top500_5';
+import Top500_6 from './pages/Top500_6';
+import Top500_7 from './pages/Top500_7';
+import Top500_8 from './pages/Top500_8';
+import Top500_9 from './pages/Top500_9';
 
 
 import MusicPlayer from './components/Music-player';
@@ -41,6 +49,15 @@ export default function App() {
             <Route path='/Search' element={<Search />}/>
             <Route path='/LoginPage' element={<LoginPage />} />
             <Route path='/MobileSearch' element={<MobileSearch/>}/>
+
+            <Route path='/Top500_2' element={<Top500_2 />}/>
+            <Route path='/Top500_3' element={<Top500_3 />}/>
+            <Route path='/Top500_4' element={<Top500_4 />}/>
+            <Route path='/Top500_5' element={<Top500_5 />}/>
+            <Route path='/Top500_6' element={<Top500_6 />}/>
+            <Route path='/Top500_7' element={<Top500_7 />}/>
+            <Route path='/Top500_8' element={<Top500_8 />}/>
+            <Route path='/Top500_9' element={<Top500_9 />}/>
           </Routes>
 
           <Footer />
