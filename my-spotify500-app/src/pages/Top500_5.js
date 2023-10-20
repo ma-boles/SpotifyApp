ab13bf47f266c9bb09559d110c127e2e774a0c0c
ma-boles <ma_boles@hotmail.com>
2023-10-20 14:29:43 -0700

    git commit -m "top500 list new file"

---
+++ my-spotify500-app/src/pages/Top500_5.js
@@ -0,0 +1,110 @@
+import "../styles/Top500.css";
+import right from "../images/chevron-right-solid.svg";
+import left from "../images/chevron-left-solid.svg";
+import Album from "../components/Album";
+
+export default function Top500_5 () {
+    return (
+        <>
+        <div className="top500--page">
+            <div className="top500--header">
+                <nav className="top500--nav">
+                    <img src={left} className='top500--arrowLeft'></img>
+                    <img src={right} className='top500--arrowRight'></img>
+                </nav>
+                
+                <h1 className="h1--top500">
+                    Top500
+                </h1>
+
+            </div>
+
+            <div className="top500">
+
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+
+            </div>
+            <div className="top500--links">
+                <ul className="top500--ul">
+                    <li className="top500--li">1-50</li>
+                    <li className="top500--li">51-100</li>
+                    <li className="top500--li">101-150</li>
+                    <li className="top500--li">151-200</li>
+                    <li className="top500--li">201-250</li>
+                    <li className="top500--li">251-300</li>
+                    <li className="top500--li">301-350</li>
+                    <li className="top500--li">351-400</li>
+                    <li className="top500--li">401-450</li>
+                    <li className="top500--li">451-500</li>
+                </ul>
+            </div>
+            <hr></hr>
+            
+        </div>
+
+        </>
+    )
+}
