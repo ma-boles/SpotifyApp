293430e9f95d35500ef2d4be5f8f485ed377bb5b
ma-boles <ma_boles@hotmail.com>
2023-10-23 17:47:15 -0700

    git commit -m "new Authentication file"

---
+++ my-spotify500-app/src/components/Authentication.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+export default function Authentication({ onTokenObtained }) {
+
+    useEffect(() => {
+        const CLIENT_ID = 'client id';
+        const CLIENT_SECRET = 'client secret';
+
+        const tokenUrl = "https://accounts.spotify.com/api/token";
+
+        //headers for POST request
+        const headers = {
+            'Authorization': 'Basic ' + btoa(`${CLIENT_ID}:${CLIENT_SECRET}`),
+            'Content-Type': 'application/x-www-form-urlencoded'
+        };
+
+        //request body
+        const body = 'grant_type=client_credentials';
+
+        //POST request to get access token 
+        fetch(tokenUrl, {
+            method: 'POST',
+            headers,
+            body
+        })
+        .then((response) => response.JSON())
+        .then((data) => {
+            const token = data.access.token;
+            onTokenObtained(token); //passes token to parent component
+        })
+        .catch((error) => {
+            console.log('Error:', error);
+        });
+    }, [onTokenObtained]);
+
+    return null;//no display
+}
