2ca7fdd467ede31afc733ef736f49b6e6748096e
ma-boles <ma_boles@hotmail.com>
2023-10-29 15:29:58 -0700

    git commit -m "changed component to custom hook"

---
+++ my-spotify500-app/src/components/useAlbumFetcher.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+/*import Album from "./Album";*/
+
+export default function useAlbumFetcher ({ accessToken, albumIds })  {
+
+    const [ albumData, setAlbumData ] = useState([]);
+    
+    useEffect(() => {
+  
+        const fetchDataForAlbum = async (albumId) => {
+          try {
+            const response = await fetch(`https://api.spotify.com/v1/albums/${albumId}`, {
+              headers: {
+                'Authorization': `Bearer ${accessToken}`,
+              },
+            });
+            
+            if(response.status === 200) {
+              const data = await response.json();
+              return data;
+            } else {
+              console.error('Error fetching album data');
+              return null;
+            }
+          } catch(error) {
+            console.error('Error:', error);
+            return null;
+          }
+        };
+  
+        // fetch data for all albums at same time using Promise.all
+        const fetchAlbums = async () => {
+          const albumPromises = albumIds.map((albumId) => fetchDataForAlbum(albumId));
+          const albumResults = await Promise.all(albumPromises);
+          setAlbumData(albumResults.filter((data) => data !== null));
+        };
+        
+        fetchAlbums();
+      }, [accessToken, albumIds]);
+  
+
+    return albumData;
+}
+
+{/*<>
+        {albumData.map((album, index) => ( 
+              <Album key={index} albumData={album}/>
+            ))}
+        </>*/}
