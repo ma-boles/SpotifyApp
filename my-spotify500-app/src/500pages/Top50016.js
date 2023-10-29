2850f984ba06cf2ddcaac09091843862b076bb31
ma-boles <ma_boles@hotmail.com>
2023-10-28 17:58:57 -0700

    git commit -m "added new 500 pages"

---
+++ my-spotify500-app/src/500pages/Top50016.js
@@ -0,0 +1,101 @@
+import { Link } from "react-router-dom";
+import "../styles/Top500.css";
+import right from "../images/chevron-right-solid.svg";
+import left from "../images/chevron-left-solid.svg";
+import Album from "../components/Album";
+
+export default function Top50016 () {
+    return (
+        <>
+        <div className="top500--page">
+            <div className="top500--header">
+                <nav className="top500--nav">
+                    <img src={left} className='top500--arrowLeft' alt="back"></img>
+                    <img src={right} className='top500--arrowRight' alt="next"></img>
+                </nav>
+                
+                <h1 className="h1--top500">
+                    Top500
+                </h1>
+
+            </div>
+
+            <div className="top500">
+
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+                <Album />                
+                <Album />
+
+            </div>
+            <div className="top500--links">
+                <ul className="top500--ul">
+
+                    <Link to="../Top500">
+                        <li className="top500--li">1-50</li>
+                    </Link>
+
+                    <Link to="../Top5002">
+                        <li className="top500--li">51-100</li>
+                    </Link>
+
+                    <Link to="../Top5003">
+                        <li className="top500--li--current">101-150</li>
+                    </Link>
+
+                    <Link to="../Top5004">
+                        <li className="top500--li">151-200</li>
+                    </Link>
+
+                    <Link to="../Top5005">
+                        <li className="top500--li">201-250</li>
+                    </Link>
+
+                    <Link to="../Top5006">
+                        <li className="top500--li">251-300</li>
+                    </Link>
+
+                    <Link to="../Top5007">
+                        <li className="top500--li">301-350</li>
+                    </Link>
+
+                    <Link to="../Top5008">
+                        <li className="top500--li">351-400</li>
+                    </Link>
+
+                    <Link to="../Top5009">
+                        <li className="top500--li">401-450</li>
+                    </Link>
+
+                        <li className="top500--li--current">451-500</li>
+                    
+                </ul>
+            </div>
+            <hr></hr>
+            
+        </div>
+
+        </>
+    )
+}
