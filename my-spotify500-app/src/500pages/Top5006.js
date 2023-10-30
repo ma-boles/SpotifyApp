082185758ad0be583c03b7126be4a4505a57c8cf
ma-boles <ma_boles@hotmail.com>
2023-10-29 18:49:31 -0700

    git commit -m "added albumIds"

--- my-spotify500-app/src/500pages/Top5006.js
+++ my-spotify500-app/src/500pages/Top5006.js
@@ -3,9 +3,15 @@ import "../styles/Top500.css";
 import right from "../images/chevron-right-solid.svg";
 import left from "../images/chevron-left-solid.svg";
 import Album from "../components/Album";
+import { useAlbumFetcher } from "../components/useAlbumFetcher";
+/*import AlbumDisplay from "../components/AlbumDisplay";*/
 import Links from "../components/Links";
 
-export default function Top5006 () {
+export default function Top5006 ({ accessToken }) {
+
+    const albumIds = ['1J8QW9qsMLx3staWaHpQmU', '49kzgMsxHU5CTeb2XmFHjo', '34LxHI9x14qXUOS8AWRrYD', '29m6DinzdaD0OPqWKGyMdz', '0Y5Wlv2OJKaW0uDJ5HnUfy', '2Rwf2nPYZQ9aIe4QXACTC7', '630o1rKTDsLeIPreOY1jqP', '3o5EnVZNJXtfPV8tCoagjI', '5SqbMEyAt8332ISGiLX0St'/*, Joni Mitchell = Court and Spark*/, '7GWkceE5McMVfffd1RGL6Y', '5WupqgR68HfuHt3BMJtgun', '5Y0p2XCgRRIjna91aQE8q7', '2k8KgmDp9oHrmu0MIj4XDE', '6PBZN8cbwkqm1ERj2BGXJ1', '0A13JySVHzBoRZFk2o89Wl', '2widuo17g5CEC66IbzveRu', '7iwS1r6JHYJe9xpPjzmWqD', '5PfnCqRbdfIDMb1x3MPQam']; //album ids from api
+    const albumData = useAlbumFetcher({ accessToken, albumIds });
+
     return (
         <>
         <div className="top500--page">
@@ -23,7 +29,12 @@ export default function Top5006 () {
 
             <div className="top500">
 
-                <Album />                
+            {/*<AlbumDisplay />*/
+}                {albumData.map((album, index) => ( 
+              <Album key={index} albumData={album}/>
+            ))}
+
+                {/*<Album />                
                 <Album />
                 <Album />                
                 <Album />
@@ -47,7 +58,7 @@ export default function Top5006 () {
                 <Album />                
                 <Album />
                 <Album />                
-                <Album />
+                <Album />*/}
 
             </div>
 
