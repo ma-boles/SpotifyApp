7670585e5b1e5ac5841ae7021ba333decac5546d
ma-boles <ma_boles@hotmail.com>
2023-10-30 16:03:08 -0700

    git commit -m "added albumIds"

--- my-spotify500-app/src/500pages/Top50011.js
+++ my-spotify500-app/src/500pages/Top50011.js
@@ -3,9 +3,15 @@ import "../styles/Top500.css";
 import right from "../images/chevron-right-solid.svg";
 import left from "../images/chevron-left-solid.svg";
 import Album from "../components/Album";
+import { useAlbumFetcher } from "../components/useAlbumFetcher";
+/*import AlbumDisplay from "../components/AlbumDisplay";*/
 import Links from "../components/Links";
 
-export default function Top50011 () {
+export default function Top50011 ({ accessToken }) {
+
+    const albumIds = ['4v5x3Oo3UjQ9YmF3hRAip5', '0HMsmYvoT1h2x1C4di5faf', '5mwOo1zikswhmfHvtqVSXg', '4SZko61aMnmgvNhfhgTuD3', '44VxbAytHpVi3Rq8hRhild', '2de6LD7eOW8zrlorbS28na', '51B7LbLWgYLKBVSpkan8Z7', '5BGzOpea6At0Nd7tYtYZOP', '7AFsTiojVaB2I58oZ1tMRg', '3RPMCDSd1yj37hlqbizmeT', '5Dgqy4bBg09Rdw7CQM545s', '2EYVXfypcucR62WMKJl6Mr', '6B5favhKTShFTjRLsKIUfa', '3ZGUBwDiY5HPOcWv4SBPQg', '7rqgm1BnAZ8I4d6hukpkdg', '5bmpvyP7UGqB4VuXmrJUMy', '3LzKUdUTdJb6P7xGN6SotC', '5eg56dCpFn32neJak2vk0f', '7btiyhWzUfzxN3ijSiBpC8', '5bHkK1X4WEOzNvRhehvOcb'] //album ids from api
+    const albumData = useAlbumFetcher({ accessToken, albumIds });
+
     return (
         <>
         <div className="top500--page">
@@ -23,7 +29,12 @@ export default function Top50011 () {
 
             <div className="top500">
 
-                <Album />                
+            {/*<AlbumDisplay />*/}
+                {albumData.map((album, index) => ( 
+              <Album key={index} albumData={album}/>
+            ))}
+
+                {/*<Album />                
                 <Album />
                 <Album />                
                 <Album />
@@ -47,7 +58,7 @@ export default function Top50011 () {
                 <Album />                
                 <Album />
                 <Album />                
-                <Album />
+                <Album />*/}
 
             </div>
 
--- my-spotify500-app/src/500pages/Top50012.js
+++ my-spotify500-app/src/500pages/Top50012.js
@@ -3,9 +3,15 @@ import "../styles/Top500.css";
 import right from "../images/chevron-right-solid.svg";
 import left from "../images/chevron-left-solid.svg";
 import Album from "../components/Album";
+import { useAlbumFetcher } from "../components/useAlbumFetcher";
+/*import AlbumDisplay from "../components/AlbumDisplay";*/
 import Links from "../components/Links";
 
-export default function Top50012 () {
+export default function Top50012 ({ accessToken }) {
+
+    const albumIds = ['4LaRYkT4oy47wEuQgkLBul', '6cuNyrSmRjBeekioLdLkvI', '0xzaemKucrJpYhyl7TltAk', '3y6G5El2I6QrJA9BdfAbqA', '0rPtXOMN42nsLDiShvGamv', '5iIWnMgvSM8uEBwXKsPcXM', '18tV6PLXYvVjsdOVk0S7M8', '5F3GEttPPR58Gg15BKUsTd'/*, Patsy Cline - The Ultimate Collection*/, '48i37aZTC1prDr4EcpQeEa', '0qKA3aqubD47jfFVoPfsiy', '7MBQWjukLxXZYvQ8vzEH7t', '1pFUGy3ABpLRRE3oNMPbDb', '7pGb2cOGVz6vLyaZaKOQ7D', '55fq75UfkYbGMq4CncCtOH', '2noRn2Aes5aoNVsU6iWThc', '5aEtg4dxdBk4pj6SJ3hNsM', '0HHRIVjvBcnTepfeRVgS2f', '3AJ15ysr4Hz5p0N2tnNb1F', '3nTXqOEHr6AfTb1WSaB4Pm'] //album ids from api
+    const albumData = useAlbumFetcher({ accessToken, albumIds });
+
     return (
         <>
         <div className="top500--page">
@@ -23,7 +29,12 @@ export default function Top50012 () {
 
             <div className="top500">
 
-                <Album />                
+            {/*<AlbumDisplay />*/}
+                {albumData.map((album, index) => ( 
+              <Album key={index} albumData={album}/>
+            ))}
+
+                {/*<Album />                
                 <Album />
                 <Album />                
                 <Album />
@@ -47,7 +58,7 @@ export default function Top50012 () {
                 <Album />                
                 <Album />
                 <Album />                
-                <Album />
+                <Album />*/}
 
             </div>
 
