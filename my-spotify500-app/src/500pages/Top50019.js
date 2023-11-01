94d4833696f850b23ce7a244f21340724bcde7fb
ma-boles <ma_boles@hotmail.com>
2023-10-31 18:29:00 -0700

    git commit -m "updated"

--- my-spotify500-app/src/500pages/Top50019.js
+++ my-spotify500-app/src/500pages/Top50019.js
@@ -1,10 +1,8 @@
-/*import { Link } from "react-router-dom";*/
 import "../styles/Top500.css";
 import right from "../images/chevron-right-solid.svg";
 import left from "../images/chevron-left-solid.svg";
-import Album from "../components/Album";
 import { useAlbumFetcher } from "../components/useAlbumFetcher";
-/*import AlbumDisplay from "../components/AlbumDisplay";*/
+import AlbumDisplay from "../components/AlbumDisplay";
 import Links from "../components/Links";
 
 export default function Top50019 ({ accessToken }) {
@@ -28,85 +26,10 @@ export default function Top50019 ({ accessToken }) {
             </div>
 
             <div className="top500">
-
-            {/*<AlbumDisplay />*/}
-                {albumData.map((album, index) => ( 
-              <Album key={index} albumData={album}/>
-            ))}
-
-                {/*<Album />                
-                <Album />
-                <Album />                
-                <Album />
-                <Album />                
-                <Album />
-                <Album />                
-                <Album />
-                <Album />                
-                <Album />
-                <Album />                
-                <Album />
-                
-                <Album />                
-                <Album />
-                <Album />                
-                <Album />
-                <Album />                
-                <Album />
-                <Album />                
-                <Album />
-                <Album />                
-                <Album />
-                <Album />                
-                <Album />*/}
-
+                <AlbumDisplay albumData={albumData}/>
             </div>
 
                 <Links />
-
-            {/*<div className="top500--links">
-                <ul className="top500--ul">
-
-                    <Link to="../Top500">
-                        <li className="top500--li">1-50</li>
-                    </Link>
-
-                    <Link to="../Top5002">
-                        <li className="top500--li">51-100</li>
-                    </Link>
-
-                    <Link to="../Top5003">
-                        <li className="top500--li--current">101-150</li>
-                    </Link>
-
-                    <Link to="../Top5004">
-                        <li className="top500--li">151-200</li>
-                    </Link>
-
-                    <Link to="../Top5005">
-                        <li className="top500--li">201-250</li>
-                    </Link>
-
-                    <Link to="../Top5006">
-                        <li className="top500--li">251-300</li>
-                    </Link>
-
-                    <Link to="../Top5007">
-                        <li className="top500--li">301-350</li>
-                    </Link>
-
-                    <Link to="../Top5008">
-                        <li className="top500--li">351-400</li>
-                    </Link>
-
-                    <Link to="../Top5009">
-                        <li className="top500--li">401-450</li>
-                    </Link>
-
-                        <li className="top500--li--current">451-500</li>
-                    
-                </ul>
-    </div>*/}
     
             <hr></hr>
             
