3bf615219bd4c383950cd4e629add1d5083bcae6
ma-boles <ma_boles@hotmail.com>
2023-11-01 18:26:43 -0700

    git commit -m "updateds to make asynchronous"

--- my-spotify500-app/src/components/useAlbumFetcher.js
+++ my-spotify500-app/src/components/useAlbumFetcher.js
@@ -27,12 +27,23 @@ export function useAlbumFetcher ({ accessToken, albumIds })  {
             return null;
           }
         };
-  
+
+        // fetch data for first batch of 10
+        const firstBatch = albumIds.slice(0, 10);
+        const firstBatchPromises = firstBatch.map((albumId) => fetchDataForAlbum(albumId));
+
+        // fetch data for second batch of 10
+        const secondBatch = albumIds.slice(10);
+        const secondBatchPromises = secondBatch.map((albumId) => 
+        new Promise ((resolve) => setTimeout(() => resolve(fetchDataForAlbum(albumId)), 1000)));
+
         // fetch data for all albums at same time using Promise.all
         const fetchAlbums = async () => {
-          const albumPromises = albumIds.map((albumId) => fetchDataForAlbum(albumId));
-          const albumResults = await Promise.all(albumPromises);
-          setAlbumData(albumResults.filter((data) => data !== null));
+        const firstBatchResults = await Promise.all(firstBatchPromises);
+        const secondBatchResults = await Promise.all(secondBatchPromises);
+
+        const albumResults = firstBatchResults.concat(secondBatchResults);
+        setAlbumData(albumResults.filter((data) => data !== null));
         };
         
         fetchAlbums();
@@ -41,3 +52,18 @@ export function useAlbumFetcher ({ accessToken, albumIds })  {
 
     return albumData;
 }
+
+/*const fetchAlbums = async () => {
+  const albumPromises = albumIds.map((albumId) => fetchDataForAlbum(albumId));
+  const albumResults = await Promise.all(albumPromises);
+  setAlbumData(albumResults.filter((data) => data !== null));
+};*/
+
+/*const albumPromises = [];
+          for (let i = 0; i < albumIds.length; i+= batchSize) {
+          const batch = albumIds.slice(i, i + batchSize);
+          const batchPromises = batch.map((albumId) => fetchDataForAlbum(albumId));
+          albumPromises.push(Promise.all(batchPromises));
+        }
+          //flatten the results array
+          const albumResults = (await Promise.all(albumPromises)).flat();*/
