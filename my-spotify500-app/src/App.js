5383fc5f6d5a41512ce022f415e572d056e13a18
ma-boles <ma_boles@hotmail.com>
2023-11-06 15:33:18 -0800

    git commit -m "added accessToken to components"

--- my-spotify500-app/src/App.js
+++ my-spotify500-app/src/App.js
@@ -1,8 +1,8 @@
 import '../src/styles/App.css';
-import React/*, { useState }*/ from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from "react-router-dom";
 
-/*import Authentication from './components/Authentication';*/
+import Authentication from './components/Authentication';
 import Decade from "../src/pages/Decade";
 import Genre from "../src/pages/Genre";
 import Podcasts from "../src/pages/Podcasts";
@@ -13,7 +13,7 @@ import MobileSearch from './pages/MobileSearch';
 
 import Top500 from "./500pages/Top500";
 import Top5002 from './500pages/Top5002';
-/*import Top5003 from './500pages/Top5003';
+import Top5003 from './500pages/Top5003';
 import Top5004 from './500pages/Top5004';
 import Top5005 from './500pages/Top5005';
 import Top5006 from './500pages/Top5006';
@@ -35,7 +35,7 @@ import Top50021 from './500pages/Top50021';
 import Top50022 from './500pages/Top50022';
 import Top50023 from './500pages/Top50023';
 import Top50024 from './500pages/Top50024';
-import Top50025 from './500pages/Top50025';*/
+import Top50025 from './500pages/Top50025';
 
 import MusicPlayer from './components/Music-player';
 import Nav from './components/Nav';
@@ -49,16 +49,16 @@ import left from "../images/chevron-left-solid.svg"*/
 
 export default function App() {
 
-  /*const [ accessToken, setAccessToken ] = useState('initial_access_token');
+  const [ accessToken, setAccessToken ] = useState('initial_access_token');
 
   const handleTokenObtained = (token) => 
-  setAccessToken(token);*/
+  setAccessToken(token);
 
 
   return (
     <>
     {/* access token */}
-    {/*<Authentication onTokenObtained={handleTokenObtained}/>*/}
+    <Authentication onTokenObtained={handleTokenObtained}/>
     
     <div className="App">
 
@@ -67,39 +67,39 @@ export default function App() {
       <div className='home--container'>
         {/* new page loads */}
           <Routes>
-            <Route path='/' element={<HomePage  /*accessToken={accessToken}*//>} />
+            <Route path='/' element={<HomePage  accessToken={accessToken}/>} />
             <Route path='/Decade' element={<Decade />} />
-            <Route path='/Top500' element={<Top500 />} />
             <Route path='/Genre' element={<Genre />} />
             <Route path='/Podcasts' element={<Podcasts />} />
             <Route path='/Search' element={<Search />}/>
          {/*<Route path='/LoginPage' element={<LoginPage />} />*/}
             <Route path='/MobileSearch' element={<MobileSearch/>}/>
 
-            <Route path='/Top5002' element={<Top5002 />}/>
-            {/*<Route path='/Top5003' element={<Top5003 />}/>
-            <Route path='/Top5004' element={<Top5004 />}/>
-            <Route path='/Top5005' element={<Top5005 />}/>
-            <Route path='/Top5006' element={<Top5006 />}/>
-            <Route path='/Top5007' element={<Top5007 />}/>
-            <Route path='/Top5008' element={<Top5008 />}/>
-            <Route path='/Top5009' element={<Top5009 />}/>
-            <Route path='/Top50010' element={<Top50010 />}/>
-            <Route path='/Top50011' element={<Top50011 />}/>
-            <Route path='/Top50012' element={<Top50012 />}/>
-            <Route path='/Top50013' element={<Top50013 />}/>
-            <Route path='/Top50014' element={<Top50014 />}/>
-            <Route path='/Top50015' element={<Top50015 />}/>
-            <Route path='/Top50016' element={<Top50016 />}/>
-            <Route path='/Top50017' element={<Top50017 />}/>
-            <Route path='/Top50018' element={<Top50018 />}/>
-            <Route path='/Top50019' element={<Top50019 />}/>
-            <Route path='/Top50020' element={<Top50020 />}/>
-            <Route path='/Top50021' element={<Top50021 />}/>
-            <Route path='/Top50022' element={<Top50022 />}/>
-            <Route path='/Top50023' element={<Top50023 />}/>
-            <Route path='/Top50024' element={<Top50024 />}/>
-  <Route path='/Top50025' element={<Top50025 />}/>*/}
+            <Route path='/Top500' element={<Top500 accessToken={accessToken}/>} />
+            <Route path='/Top5002' element={<Top5002 accessToken={accessToken}/>}/>
+            <Route path='/Top5003' element={<Top5003 accessToken={accessToken}/>}/>
+            <Route path='/Top5004' element={<Top5004 accessToken={accessToken}/>}/>
+            <Route path='/Top5005' element={<Top5005 accessToken={accessToken}/>}/>
+            <Route path='/Top5006' element={<Top5006 accessToken={accessToken}/>}/>
+            <Route path='/Top5007' element={<Top5007 accessToken={accessToken}/>}/>
+            <Route path='/Top5008' element={<Top5008 accessToken={accessToken}/>}/>
+            <Route path='/Top5009' element={<Top5009 accessToken={accessToken}/>}/>
+            <Route path='/Top50010' element={<Top50010 accessToken={accessToken}/>}/>
+            <Route path='/Top50011' element={<Top50011 accessToken={accessToken}/>}/>
+            <Route path='/Top50012' element={<Top50012 accessToken={accessToken}/>}/>
+            <Route path='/Top50013' element={<Top50013 accessToken={accessToken}/>}/>
+            <Route path='/Top50014' element={<Top50014 accessToken={accessToken}/>}/>
+            <Route path='/Top50015' element={<Top50015 accessToken={accessToken}/>}/>
+            <Route path='/Top50016' element={<Top50016 accessToken={accessToken}/>}/>
+            <Route path='/Top50017' element={<Top50017 accessToken={accessToken}/>}/>
+            <Route path='/Top50018' element={<Top50018 accessToken={accessToken}/>}/>
+            <Route path='/Top50019' element={<Top50019 accessToken={accessToken}/>}/>
+            <Route path='/Top50020' element={<Top50020 accessToken={accessToken}/>}/>
+            <Route path='/Top50021' element={<Top50021 accessToken={accessToken}/>}/>
+            <Route path='/Top50022' element={<Top50022 accessToken={accessToken}/>}/>
+            <Route path='/Top50023' element={<Top50023 accessToken={accessToken}/>}/>
+            <Route path='/Top50024' element={<Top50024 accessToken={accessToken}/>}/>
+            <Route path='/Top50025' element={<Top50025 accessToken={accessToken}/>}/>
 
           </Routes>
 
