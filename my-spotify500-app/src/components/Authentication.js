1e86f6bc8c83e7fcacf2f1e5a65bfdd9ca4024e2
ma-boles <ma_boles@hotmail.com>
2023-11-05 18:24:08 -0800

    git commit -m "removed client_id/client_secret"

--- my-spotify500-app/src/components/Authentication.js
+++ my-spotify500-app/src/components/Authentication.js
@@ -1,27 +1,14 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
 export default function Authentication({ onTokenObtained }) {
 
     useEffect(() => {
-        const CLIENT_ID = '';
-        const CLIENT_SECRET = '';
 
         const tokenUrl = "http://localhost:3001/authenticate";
-
-        //headers for POST request
-        const headers = {
-            'Authorization': 'Basic ' + btoa(`${CLIENT_ID}:${CLIENT_SECRET}`),
-            'Content-Type': 'application/x-www-form-urlencoded'
-        };
-
-        //request body
-        const body = 'grant_type=client_credentials';
 
         //POST request to get access token 
         fetch(tokenUrl, {
             method: 'POST',
-            headers,
-            body
         })
         .then(response => response.json())
         .then(data => {
@@ -35,3 +22,4 @@ export default function Authentication({ onTokenObtained }) {
 
     return null;//no display
 }
+
