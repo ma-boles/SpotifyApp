a5f661845ac00048965dae2fb4f8cc192f2be625
ma-boles <ma_boles@hotmail.com>
2023-11-06 18:23:06 -0800

    git commit -m "updated code"

--- my-spotify500-app/src/components/Podcast.js
+++ my-spotify500-app/src/components/Podcast.js
@@ -2,16 +2,16 @@ import React from "react";
 import "../styles/Albums.css";
 
 
-export default function Podcast() {
+export default function Podcast( podcastData ) {
     return(
         <>
-            {/*<div className="card--album">
-                <img style={styles} src={showData.images[0].url} alt={showData.name} className="album--cover" ></img>
+            <div className="card--album">
+                <img style={styles} src={podcastData.images[0].url} alt={podcastDataData.name} className="album--cover" ></img>
                 <div className="album--info">
-                    <h2 className="album--h2">{showData.name}</h2>
-                    <h3 className="album--h3">{showData.publisher[0]}</h3>
+                    <h2 className="album--h2">{podcastData.name}</h2>
+                    <h3 className="album--h3">{podcastData.publisher[0]}</h3>
                 </div>
-            </div>*/}
+            </div>
         </>
     )
 }
