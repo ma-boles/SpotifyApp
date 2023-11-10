cdca66ea9dc723dd9a0a78f28a6288190bbb495b
ma-boles <ma_boles@hotmail.com>
2023-11-09 18:21:48 -0800

    git commit -m "updated podcast fetch"

--- my-spotify500-app/src/components/Podcast.js
+++ my-spotify500-app/src/components/Podcast.js
@@ -3,15 +3,21 @@ import "../styles/Albums.css";
 
 
 export default function Podcast({ podcastData }){
+    if(!podcastData || !podcastData.images || podcastData.images.length === 0) { // handles the case where podcastData or images are not defined or empty
+        return null;
+    }
     return(
         <>
             <div className="card--album">
                 <img src={podcastData.images[0].url} alt={podcastData.name} className="album--cover" ></img>
                 <div className="album--info">
                     <h2 className="album--h2">{podcastData.name}</h2>
-                    <h3 className="album--h3">{podcastData.publisher[0]}</h3>
+                    {podcastData.publisher[0] && (
+                        <h3 className="album--h3">{podcastData.publisher[0]}</h3>
+                    )}
+                    
                 </div>
             </div>
         </>
-    )
+    );
 }
