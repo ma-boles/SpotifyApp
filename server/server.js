f755258a371e459a856783d8ff7114131542e62a
ma-boles <ma_boles@hotmail.com>
2023-11-12 18:32:38 -0800

    git commit -m "changing authorization type"

--- server/server.js
+++ server/server.js
@@ -5,54 +5,60 @@ import { CLIENT_ID, CLIENT_SECRET } from './config.js'
 
 const app = express();
 const port = process.env.PORT || 3001;
+const redirectUri = 'https://your-redirect-uri/callback';
 
 app.use(cors()); //enable CORS for all routes
 app.use(express.json()); //parse json request bodies
 
-// authentication 
-app.post('/authenticate', (req, res) => {
-    
-    /* CLIENT_ID = config.CLIENT_ID;
-     CLIENT_SECRET = config.CLIENT_SECRET;*/
+app.get('/login', (req, res) => {
+    const scope = 'user-read-playback-state user-modify-playback-state'; //include necessary scopes
+    const state = generateRandomString(16);
+    const authorizeUrl = `https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&scope=${encodeURIComponent(scope)}&redirect_uri=${encodeURIComponent(redirectUri)}&state=${state}`;
+    res.redirect(authorizeUrl);
+});
 
-    const tokenUrl = "https://accounts.spotify.com/api/token";
+app.get('/callback', async (req, res) => {
+    const { code, state } = req.query;
 
-    //headers for POST request
+    //verify state to prevent csrf attacks
+
+    //exchange authorization code for access token
+    const tokenUrl = 'https://accounts.spotify.com/api/token';
+    const body = `grant_type=authorization_code&code=${code}&redirect_uri=${encodeURIComponent(redirectUri)}`;
     const headers = {
         'Authorization': `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
         'Content-Type': 'application/x-www-form-urlencoded'
     };
 
-    //request body
-    const body = 'grant_type=client_credentials';
+    try {
+        const response = await fetch(tokenUrl, {
+            method: 'POST',
+            headers,
+            body,
+        });
 
-    //POST request to get access token 
-    fetch(tokenUrl, {
-        method: 'POST',
-        headers,
-        body
-    })
-    .then(response => {
-        if(response.ok) {
-            return response.json();
-        } else {
+        if(!response.ok) {
             throw new Error('Failed to obtain access token');
         }
-    }) 
-    .then(data => {
-        const token = data.access_token;
-        res.json({ token });
-    })
-    .catch(error => {
+
+        const data = await response.json();
+        const accessToken = data.access_token;
+
+        //pass the access token to the client
+        res.json({ accessToken });
+    } catch (error) {
         console.error('Error:', error);
-        res.status(400).json({ error: 'Authentication failed' });    
-    })
-      
+        res.status(400).json({ error: 'Authentication failed'});
+    }
 });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+function generateRandomString(length) {
+    
+}
+
 
 
 /*'Authorization': 'Basic ' + Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64'),
@@ -66,4 +72,57 @@ app.listen(port, () => {
     console.log('Error:', error);
     res.status(500).json({ error: 'Authentication failed' });
 });
-});*/
+});
+
+
+
+
+
+
+
+// authentication 
+app.post('/authenticate', (req, res) => {
+    
+    /* CLIENT_ID = config.CLIENT_ID;
+     CLIENT_SECRET = config.CLIENT_SECRET;*/
+
+     /*const tokenUrl = "https://accounts.spotify.com/api/token";*/
+
+     //headers for POST request
+    /* const headers = {
+         'Authorization': `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+         'Content-Type': 'application/x-www-form-urlencoded'
+     };*/
+ 
+     //request body
+    /* const body = 'grant_type=client_credentials';*/
+ 
+     //POST request to get access token 
+    /* fetch(tokenUrl, {
+         method: 'POST',
+         headers,
+         body
+     })
+     .then(response => {
+         if(response.ok) {
+             return response.json();
+         } else {
+             throw new Error('Failed to obtain access token');
+         }
+     }) 
+     .then(data => {
+         const token = data.access_token;
+         res.json({ token });
+     })
+     .catch(error => {
+         console.error('Error:', error);
+         res.status(400).json({ error: 'Authentication failed' });    
+     })
+       
+ });
+ 
+ app.listen(port, () => {
+     console.log(`Server is running on port ${port}`);
+ });
+ 
+ */
