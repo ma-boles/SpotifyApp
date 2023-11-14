816cfaea7f00c98eee6a15af7edb029a2738f2e3
ma-boles <ma_boles@hotmail.com>
2023-11-13 18:09:34 -0800

    git commit -m "implemented login"

--- my-spotify500-app/src/components/Authentication.js
+++ my-spotify500-app/src/components/Authentication.js
@@ -4,22 +4,11 @@ export default function Authentication({ onTokenObtained }) {
 
     useEffect(() => {
 
-        const tokenUrl = "http://localhost:3001/authenticate";
+        const authenticateUrl = "http://localhost:3001/login";
 
-        //POST request to get access token 
-        fetch(tokenUrl, {
-            method: 'POST',
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            const token = data.token;
-            onTokenObtained(token); //passes token to parent component
-        })
-        .catch((error) => {
-            console.log('Error:', error);
-        });
-    }, [onTokenObtained]);
+        //redirect the user to the Spotify login
+        window.location.href = authenticateUrl;
+    }, []);
 
     return null;//no display
 }
-
